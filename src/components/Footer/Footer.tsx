b399cc9a39f3fe8e00f9f526bc8a3058470b6963
sanidhyasin <singhsanidhya741@gmail.com>
2024-08-11 08:15:55 +0530

    fotter changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,44 +2,40 @@
 
 import { ActionIcon, Anchor, Container, Group, Text } from "@mantine/core";
 import {
-  IconBrandInstagram,
-  IconBrandTwitter,
-  IconBrandYoutube,
+	IconBrandInstagram,
+	IconBrandTwitter,
+	IconBrandYoutube,
 } from "@tabler/icons-react";
 import classes from "./Footer.module.css";
 
 export function Footer() {
-  return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <Text c="dimmed" fz="sm">
-          Build by{" "}
-          <Anchor href="https://github.com/jotyy" size="sm">
-            jotyy
-          </Anchor>
-          . Hosted on{" "}
-          <Anchor href="https://vercel.com" size="sm">
-            Vercel
-          </Anchor>
-          .
-        </Text>
-        <Group
-          gap={0}
-          className={classes.links}
-          justify="flex-end"
-          wrap="nowrap"
-        >
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter size="1.05rem" stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube size="1.05rem" stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram size="1.05rem" stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </div>
-  );
+	return (
+		<div className={classes.footer}>
+			<Container className={classes.inner}>
+				<Text c="dimmed" fz="sm">
+					Hosted on{" "}
+					<Anchor href="https://vercel.com" size="sm">
+						Vercel
+					</Anchor>
+					.
+				</Text>
+				<Group
+					gap={0}
+					className={classes.links}
+					justify="flex-end"
+					wrap="nowrap"
+				>
+					<ActionIcon size="lg" color="gray" variant="subtle">
+						<IconBrandTwitter size="1.05rem" stroke={1.5} />
+					</ActionIcon>
+					<ActionIcon size="lg" color="gray" variant="subtle">
+						<IconBrandYoutube size="1.05rem" stroke={1.5} />
+					</ActionIcon>
+					<ActionIcon size="lg" color="gray" variant="subtle">
+						<IconBrandInstagram size="1.05rem" stroke={1.5} />
+					</ActionIcon>
+				</Group>
+			</Container>
+		</div>
+	);
 }
